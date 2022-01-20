ce78c80a4bb3f5ba6b52bfec931c6adc066bf437
Tomislav Cajic <tcajic00@fesb.hr>
2022-01-20 22:08:50 +0100

    Add basic onClick functionality for card button

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,17 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:spica_app/shared/navigation_bar_top.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  bool isOpen = false;
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: PreferredSize(
+      appBar: const PreferredSize(
         preferredSize: Size.fromHeight(80),
         child: NavigationBar(),
       ),
-      drawer: Text("drawer"),
+      drawer: const Text("drawer"),
       body: Column(
         children: [
           SizedBox(
@@ -42,17 +48,87 @@ class HomeScreen extends StatelessWidget {
             width: double.infinity,
             height: 80,
             child: ElevatedButton(
+              onPressed: cardVisibilitySwitch,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      Text(
+                        "COMPANY",
+                        style: TextStyle(
+                          color: Colors.grey[600],
+                          fontSize: 20,
+                        ),
+                      ),
+                      Text(
+                        "NAME 1",
+                        style: TextStyle(
+                            color: Colors.grey[800],
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                  Icon(
+                    isOpen ? Icons.expand_less : Icons.expand_more,
+                    color: Colors.grey[600],
+                    size: 30,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Visibility(
+            child: Text("card1"),
+            maintainSize: true,
+            maintainAnimation: true,
+            maintainState: true,
+            visible: isOpen,
+          ),
+          SizedBox(
+            width: double.infinity,
+            height: 80,
+            child: ElevatedButton(
               onPressed: () {},
-              child: Text(
-                "card",
-                style: TextStyle(
-                  color: Colors.black,
-                ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      Text(
+                        "COMPANY",
+                        style: TextStyle(
+                          color: Colors.grey[600],
+                          fontSize: 20,
+                        ),
+                      ),
+                      Text(
+                        "NAME 2",
+                        style: TextStyle(
+                            color: Colors.grey[800],
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                  Icon(
+                    Icons.expand_more,
+                    color: Colors.grey[600],
+                    size: 30,
+                  ),
+                ],
               ),
             ),
           ),
         ],
       ),
     );
+  }
+
+  void cardVisibilitySwitch() {
+    setState(() {
+      isOpen ? isOpen = false : isOpen = true;
+    });
   }
 }
