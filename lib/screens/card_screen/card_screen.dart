62dcfa75f210881a25e4be27e60983fbfc1f14c1
Tomislav Cajic <tcajic00@fesb.hr>
2022-01-22 15:56:43 +0100

    Finished drawer and modified routes logic

--- lib/screens/card_screen/card_screen.dart
+++ lib/screens/card_screen/card_screen.dart
@@ -23,106 +23,131 @@ class _CardScreenState extends State<CardScreen> {
         child: NavigationBar(),
       ),
       drawer: SideMenu(),
-      body: SizedBox(
-        height: 300,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(
-            vertical: 15.0,
-            horizontal: 200.0,
-          ),
-          child: Center(
-            child: Container(
-              width: 500,
-              decoration: BoxDecoration(
-                borderRadius: const BorderRadius.all(
-                  Radius.circular(25),
-                ),
-                color: Colors.pink[300],
+      body: Column(
+        children: [
+          SizedBox(
+            height: 300,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                vertical: 15.0,
+                horizontal: 200.0,
               ),
-              child: Padding(
-                padding: const EdgeInsets.all(12.0),
-                child: Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: const [
-                        Text(
-                          "SPICACARD",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 32,
+              child: Center(
+                child: Container(
+                  width: 500,
+                  decoration: BoxDecoration(
+                    borderRadius: const BorderRadius.all(
+                      Radius.circular(25),
+                    ),
+                    color: Colors.pink[300],
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: const [
+                            Text(
+                              "SPICACARD",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 32,
+                              ),
+                            ),
+                            Icon(
+                              Icons.nfc_outlined,
+                              color: Colors.white,
+                              size: 50,
+                            ),
+                          ],
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 20),
+                          child: Row(
+                            children: [
+                              Text(
+                                widget.data.username,
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20,
+                                ),
+                              ),
+                            ],
                           ),
                         ),
-                        Icon(
-                          Icons.nfc_outlined,
-                          color: Colors.white,
-                          size: 50,
-                        ),
-                      ],
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 20),
-                      child: Row(
-                        children: [
-                          Text(
-                            widget.data.username,
+                        Padding(
+                          padding: EdgeInsets.only(
+                            top: 10.0,
+                            bottom: 30,
+                          ),
+                          child: Text(
+                            widget.data.cardNumber,
                             style: TextStyle(
                               color: Colors.white,
-                              fontSize: 20,
+                              fontSize: 50,
                             ),
                           ),
-                        ],
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(
-                        top: 10.0,
-                        bottom: 30,
-                      ),
-                      child: Text(
-                        widget.data.cardNumber,
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 50,
                         ),
-                      ),
-                    ),
-                    Row(
-                      children: const [
-                        Text(
-                          "VALID UNTIL",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 15,
-                          ),
+                        Row(
+                          children: const [
+                            Text(
+                              "VALID UNTIL",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 15,
+                              ),
+                            ),
+                          ],
                         ),
-                      ],
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          widget.data.validUntil,
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 20,
-                          ),
-                        ),
-                        Text(
-                          widget.data.company + widget.data.name,
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 20,
-                          ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              widget.data.validUntil,
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                              ),
+                            ),
+                            Text(
+                              widget.data.company + widget.data.name,
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                              ),
+                            ),
+                          ],
                         ),
                       ],
                     ),
-                  ],
+                  ),
                 ),
               ),
             ),
           ),
-        ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("SPICA ",
+                  style: TextStyle(
+                    color: Colors.grey[600],
+                    fontSize: 30,
+                  )),
+              Text("DAILY",
+                  style: TextStyle(
+                    color: Colors.grey[800],
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30,
+                  )),
+              Text(" REPORT",
+                  style: TextStyle(
+                    color: Colors.grey[600],
+                    fontSize: 30,
+                  ))
+            ],
+          ),
+        ],
       ),
     );
   }
--- lib/shared/side_menu.dart
+++ lib/shared/side_menu.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:spica_app/data/card_data.dart';
-import 'package:spica_app/screens/card_screen/card_screen.dart';
-import 'package:spica_app/screens/home_screen/home_screen.dart';
+import 'package:flutter/services.dart';
 
 class SideMenu extends StatefulWidget {
   const SideMenu({Key? key}) : super(key: key);
@@ -11,19 +9,95 @@ class SideMenu extends StatefulWidget {
 }
 
 class _SideMenuState extends State<SideMenu> {
-  CardData currentCard = CardData("", "", "", "", "", 0);
   @override
   Widget build(BuildContext context) {
     return Drawer(
       child: ListView(children: [
-        const DrawerHeader(
-          child: Text("SPICAPP"),
+        DrawerHeader(
+          decoration: BoxDecoration(color: Colors.pink[100]),
+          child: Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(right: 8),
+                child: Icon(
+                  Icons.language,
+                  size: 50,
+                  color: Colors.red[700],
+                ),
+              ),
+              Text(
+                "SPIC",
+                style: TextStyle(
+                  color: Colors.grey[600],
+                  fontSize: 30,
+                ),
+              ),
+              Text(
+                "A",
+                style: TextStyle(
+                  color: Colors.grey[800],
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                "PP",
+                style: TextStyle(
+                  color: Colors.grey[600],
+                  fontSize: 30,
+                ),
+              ),
+            ],
+          ),
         ),
         ListTile(
-          title: const Text("HOME"),
+          leading: const Icon(
+            Icons.credit_card,
+            size: 30,
+          ),
+          title: Text(
+            "CARD LIST",
+            style: TextStyle(
+              color: Colors.grey[600],
+              fontSize: 20,
+            ),
+          ),
           onTap: () {
             Navigator.pop(context);
             Navigator.pushNamed(context, '/');
+          },
+        ),
+        ListTile(
+          leading: const Icon(
+            Icons.analytics,
+            size: 30,
+          ),
+          title: Text(
+            "ANALYTICS",
+            style: TextStyle(
+              color: Colors.grey[600],
+              fontSize: 20,
+            ),
+          ),
+          onTap: () {
+            Navigator.pop(context);
+            Navigator.pushNamed(context, '/analytics');
+          },
+        ),
+        ListTile(
+          leading: const Icon(
+            Icons.exit_to_app,
+            size: 30,
+          ),
+          title: Text(
+            "EXIT",
+            style: TextStyle(
+              color: Colors.grey[600],
+              fontSize: 20,
+            ),
+          ),
+          onTap: () {
+            SystemNavigator.pop();
           },
         ),
       ]),
